dd4b0b62290442d827e8741fa09ee39ad42b5476
CaraNetzer <cara.netzer@gmail.com>
2025-10-18 22:15:37 -0400

    adjusted library and projects layout

--- jsx/personal_website/src/app/library/page.js
+++ jsx/personal_website/src/app/library/page.js
@@ -5,27 +5,27 @@ export default function Page() {
         <>
             <title>Library - CaraCodes</title>
             <h1>Library</h1>
-            <p>A list of books that have contributed to my skill stack</p>
+            <p id="library-label">A list of books that have contributed to my work and career</p>
             <div className="library">
                 <div className="book">
                     <img src="/csharpcookbook.jpg" alt="csharp cookbook book cover" />
-                    <p>C# Cookbook - Completed</p>
+                    <p><b>C# Cookbook</b><br/>Completed 2023</p>
                 </div>
                 <div className="book">
                     <img src="/nand2tetris.jpg" alt="nand2tetris book cover" />
-                    <p>Nand2Tetris - Completed</p>
+                    <p><b>Nand2Tetris</b><br/>Completed 2023-2024</p>
                 </div>
                 <div className="book">
                     <img src="/eloquentjavascript.jpg" alt="eloquent javascript book cover" />
-                    <p>Eloquent Javascript - Completed</p>
+                    <p><b>Eloquent Javascript</b><br/>Completed 2024</p>
                 </div>
                 <div className="book">
                     <img src="/softwarearchitecturepatterns.jpg" alt="software architecture patterns book cover" />
-                    <p>Software Architecture Patterns - Completed</p>
+                    <p><b>Software Architecture Patterns</b><br/>Completed 2025</p>
                 </div>
                 <div className="book">
                     <img src="/technicalwritingforsoftwaredevelopers.jpg" alt="technical writing for software developers book cover" />
-                    <p>Technical Writing for Software Developers - Completed</p>
+                    <p><b>Technical Writing for Software Developers</b><br/>Completed 2025</p>
                 </div>
             </div>
         </>
