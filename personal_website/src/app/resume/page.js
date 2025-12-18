7855ddf33dd436704e8cab74d6ad8af0a515bcb3
cnetzer <cara.netzer@gmail.com>
2025-12-18 10:23:02 -0500

    security update + removed course and updated resume text

--- personal_website/src/app/resume/page.js
+++ personal_website/src/app/resume/page.js
@@ -11,11 +11,13 @@ export default function Resume() {
                 <h2>WV Higher Education Policy Commission</h2>
                 <p>Hybrid / Charleston, WV </p>
                 <ul>
-                    <li>Maintain and develop ASP.NET Web Forms applications in C#.</li>
-                    <li>Write back-end PL/SQL procedures.</li>
-                    <li>Spearheaded the introduction of git version control into our workflow.</li>
-                    <li>Participate in rounds of development and testing with a team.</li>
-                    <li>Review, analyze, and communicate with users about data submitted via our applications.</li>
+                    <li>Develop, maintain, and test ASP.NET Web Forms applications with a team using C# and PL/SQL</li>
+                    <li>Rearchitected a disparate suite of middleware integrations into one comprehensive application in order to reduce redundancies</li>
+                    <li>Spearheaded the introduction of Git version control and continue to anchor its usage on our team</li>
+                    <li>Created an automated documentation process and application for project configuration and reference material</li>
+                    <li>Extract, clean, and transform data from internal systems to create reports and dashboards</li>
+                    <li>Review, analyze, and communicate with users about data submitted via our applications</li>
+
                 </ul>
             </div>
 
@@ -26,16 +28,13 @@ export default function Resume() {
                 <p>Remote / Charleston, WV </p>
                 <ul>
                     <li>Applied object-oriented programming fundamentals through team-based projects that reflect real world business problems</li>
-                    <li>Collaborated remotely on projects using Slack and Zoom</li>
-                    <li>Managed source code version control with Git/ GitHub</li>
+                    <li>Designed applications through white boarding dependencies and building ERDs</li>
+                    <li>Built and interacted with relational databases using SQL Server and ADO.NET</li>
+                    <li>Designed and built a Podcast Recommender app in combination with the Spotify API with React using hooks, as well as a video game sprite organizer app using a RESTful Web API with C#/.NET Core, connected to a React front-end</li>
+                    <li>Gained experience with ASP.NET, MVC, and Razor templates in Visual Studio 2022, and built and maintained unit tests for a .NET Web API</li>
+                    <li>Managed source code version control with Git/GitHub</li>
                     <li>Applied JavaScript, HTML, and CSS fundamentals to build a feature-rich social media dashboard</li>
                     <li>Leveraged native ES6 module bundling to build DRY, reusable components</li>
-                    <li>Designed and built a Podcast Recommender app in combination with the Spotify API with React using hooks</li>
-                    <li>Designed applications through white boarding dependencies and building ERD’s</li>
-                    <li>Built and interacted with databases using SQL and ADO.NET</li>
-                    <li>Developed a blog management platform in ASP.NET, MVC, and Razor templates in Visual Studio 2022</li>
-                    <li>Created RESTful Web API with C#/ .NET Core and connected it to a React front-end</li>
-                    <li>Built and maintained unit tests in a .NET Web API</li>
                 </ul>
             </div>
 
@@ -45,12 +44,11 @@ export default function Resume() {
                 <h2>Public Defender Services - Public Defense Resource Specialist</h2>
                 <p>Charleston, WV</p>
                 <ul>
-                    <li>Coordinated a website redesign process, which included collaborating with the development team, using SharePoint and some custom HTML and CSS, and maintaining general website upkeep for the office.</li>
-                    <li>Restructured and automated the Personnel Change system to streamline personnel updates and subsequent data analysis.</li>
-                    <li>Developed a ResourceHub app and Personnel Tracker app with Google AppSheet</li>
+                    <li>Coordinated website redesign process using SharePoint and custom HTML/CSS, collaborated with a development team, maintained a job board on the finished site</li>
+                    <li>Using Google AppSheet, developed a ResourceHub app and restructured/automated the Personnel Change system to streamline personnel updates and subsequent data analysis</li>
+                    <li>Analyzed outcomes for and engaged in periodic reevaluation of all programs, including data collection tools, past and future goals, and internal and external reception</li>
                     <li>Collaborated with teams to manage (1) our social work program, (2) programming related to juvenile law, and (3) our annual conference.</li>
-                    <li>Engaged in periodic reevaluation of all programs, including data collection tools, past and future goals, and internal and external reception.</li>
-                    <li>Assisted in training social workers from around the state in mitigation, promoted the use of social workers among attorneys, maintained a job board on our website, supported hired social workers, and analyzed program outcomes using Microsoft Excel and Google Sheets.</li>
+                    <li>Assisted in training social workers from around the state in mitigation, promoted the use of social workers among attorneys, supported hired social workers, and analyzed program outcomes using Microsoft Excel and Google Sheets.</li>
                 </ul>
             </div>
 
